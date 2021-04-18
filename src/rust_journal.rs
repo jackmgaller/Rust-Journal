939d71f70bcce8309e6c1d401b5d5a9f82545995
Jack Galler <gallerdude@Jacks-Mac-mini.local>
2021-04-18 10:45:22 -0500

    print_one, print_all

--- src/rust_journal.rs
+++ src/rust_journal.rs
@@ -1,35 +1,49 @@
 use std::fs;
 use std::collections::HashMap;
+use std::env;
 
 fn main() {
-  let mut tags: HashMap<String, Vec<String>> = HashMap::new();
 
+  let args: Vec<String> = env::args().collect();
+
+
+  let mut tags: HashMap<String, Vec<String>> = HashMap::new();
   import_tags(&mut tags);
 
   let file_names = get_file_names();
 
-  for file_name in file_names {
-    println!("{}", file_name);
-
-    print_file_metadata(&file_name);
-    print_file_data(&file_name);
-
-    if tags.contains_key(&file_name) {
-      print!("Tags: ");
-      let file_tags: Vec<String> = tags.get(&file_name).unwrap().to_vec();
-
-      for (i, file_tag) in file_tags.iter().enumerate() {
-        if i != file_tags.len() - 1 {
-          print!("{}, ", file_tag);
-        } else {
-          println!("{}", file_tag);
-        }
-      }
-      println!("");
+  for (i, arg) in args.iter().enumerate() {
+    if arg == "--print-all" {
+      print_all(&file_names, &tags);
+    } else if arg == "--print" {
+      print_one(&args[i + 1].as_str(), &file_names, &tags);
     }
   }
 
 }
+
+fn print_one(file_search: &str, file_names: &Vec<String>, tags: &HashMap<String, Vec<String>>) {
+  for file_name in file_names {
+    if file_name.as_str().contains(file_search) {
+      println!("{}", file_name);
+      print_file_metadata(&file_name);
+      print_file_data(&file_name);
+      print_file_tags(&file_name, &tags);
+      break;
+    }
+  }
+}
+
+fn print_all(file_names: &Vec<String>, tags: &HashMap<String, Vec<String>>) {
+  for file_name in file_names {
+    println!("{}", file_name);
+    print_file_metadata(&file_name);
+    print_file_data(&file_name);
+    print_file_tags(&file_name, &tags);
+  }
+}
+  
+
 
 fn get_file_names() -> Vec<String> {
   let files = fs::read_dir("data")
@@ -67,4 +81,22 @@ fn print_file_metadata(path: &String) {
 fn print_file_data(path: &String) {
   let data = fs::read_to_string(path).unwrap();
   println!("{}", data);  
+}
+
+fn print_file_tags(file_name: &String, tags: &HashMap<String, Vec<String>>) {
+  if tags.contains_key(file_name) {
+    print!("Tags: ");
+    let file_tags: Vec<String> = tags.get(file_name).unwrap().to_vec();
+
+    for (i, file_tag) in file_tags.iter().enumerate() {
+      if i != file_tags.len() - 1 {
+        print!("{}, ", file_tag);
+      } else {
+        println!("{}", file_tag);
+      }
+    }
+    println!("");
+  } else {
+    println!("No tags for {}", file_name);
+  }
 }
