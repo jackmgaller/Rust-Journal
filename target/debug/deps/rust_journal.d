77804bb775bfc7c924c6ea276a468a34135752e7
Jack Galler <gallerdude@Jacks-Mac-mini.local>
2021-04-18 09:44:00 -0500

    cache goofiness

--- target/debug/deps/rust_journal.d
+++
@@ -1,5 +0,0 @@
-/Users/gallerdude/Projects/2021-04 Rust Journal/target/debug/deps/rust_journal: src/rust_journal.rs
-
-/Users/gallerdude/Projects/2021-04 Rust Journal/target/debug/deps/rust_journal.d: src/rust_journal.rs
-
-src/rust_journal.rs:
--- target/debug/rust_journal.d
+++
@@ -1,1 +0,0 @@
-/Users/gallerdude/Projects/2021-04\ Rust\ Journal/target/debug/rust_journal: /Users/gallerdude/Projects/2021-04\ Rust\ Journal/src/rust_journal.rs
